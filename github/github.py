d805046e27cbe147557d494d22b529b366a6c8c5
Dav Glass <davglass@daveglass.com>
2008-12-05 23:35:55 -0800

    Added the ability to define the github API token in the trac.ini

--- github/github.py
+++ github/github.py
@@ -1,13 +1,15 @@
 from trac.core import *
+from trac.config import Option, IntOption, ListOption, BoolOption
 from trac.web import IRequestHandler
 from hook import CommitHook
-import json
-
-GITHUB_KEY = '9953d62bbc3905491971513876218f39'
+import simplejson
 
 class GithubPlugin(Component):
     implements(IRequestHandler)
     
+    GITHUB_KEY = Option('github', 'apitoken', '', 
+        doc="""Your GitHub API Token found here: https://github.com/account, """)
+
     def __init__(self):
         self.hook = CommitHook(self.env)
     
@@ -17,7 +19,7 @@ class GithubPlugin(Component):
     
     def process_request(self, req):
         try:
-            data = json.read(req.read())
+            data = simplejson.loads(req.read())
 
             for sha1, commit in data['commits'].items():
                 self.hook.process(commit)
